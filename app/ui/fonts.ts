519614c322d2d341c6229ab7cc94ceeaba27fa8d
Grapple228 <greenapple228882@yandex.ru>
2024-09-08 18:15:53 +0700

    Init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import {Inter} from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin'],
+})
+
+export const lusitana = Inter({
+    subsets: ['latin'],
+    variable: '--font-lusitana',
+    weight: '700',
+    display: 'swap',
+})
